cbfa65b8b619c67f4a623fa014156492d44eb00d
christianjuniadi252-ai <christianjuniadi252@gmail.com>
2026-06-29 16:50:22 +0700

    update app.js

--- app.js
+++ app.js
@@ -171,6 +171,20 @@ function getYoutubeId(input) {
     }
 }
 
+async function getVideoInfo(videoId){
+
+    const res = await fetch(
+        `https://noembed.com/embed?url=https://www.youtube.com/watch?v=${videoId}`
+    );
+
+    const data = await res.json();
+
+    return {
+        title: data.title || "Video YouTube"
+    };
+
+}
+
 async function getVideoDuration(videoId){
 
     return new Promise(resolve=>{

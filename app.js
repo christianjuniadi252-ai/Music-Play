45ed3b3ed541ed15a576e48a29aeb3b34831d306
christianjuniadi252-ai <christianjuniadi252@gmail.com>
2026-06-29 11:40:10 +0700

    update app.js

--- app.js
+++ app.js
@@ -670,7 +670,9 @@ onSnapshot(q, snapshot => {
             
                 ` : `
             
-                    <span class="message-content">${msg.message}</span>
+                  <span class="message-content ${msg.edited ? 'edited-content' : ''}">
+                      ${msg.message}
+                  </span>
             
                 `}
             

95c43ef996810ebe6e772a0876b70255e6a83fca
christianjuniadi252-ai <christianjuniadi252@gmail.com>
2026-07-04 21:58:54 +0700

    update app.js

--- app.js
+++ app.js
@@ -716,15 +716,15 @@ async function sendMessage() {
             
             }
 
-                const roomSnap = await getDoc(roomRef);
-                
-                const room = roomSnap.exists() ? roomSnap.data() : null;
-                
-                if (
-                    !room ||
-                    !room.videoId ||
-                    room.status === "stopped"
-                ) {
+            const roomSnap = await getDoc(roomRef);
+            
+            const room = roomSnap.exists() ? roomSnap.data() : null;
+            
+            if (
+                !room ||
+                !room.videoId ||
+                room.status === "stopped"
+            ) {
 
                 await setDoc(roomRef, {
                     videoId: id,

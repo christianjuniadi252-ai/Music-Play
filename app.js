5dc24b0bd5ee2e695ef9c7eaca68dbbeebb89511
christianjuniadi252-ai <christianjuniadi252@gmail.com>
2026-07-05 15:17:44 +0700

    update app.js

--- app.js
+++ app.js
@@ -1155,7 +1155,7 @@ if(e.target === menuOverlay){
 const q = query(
     collection(db, "messages"),
     orderBy("timestamp", "desc"),
-    limit(10)
+    limit(100)
 );
 
 let previousUid = "";
@@ -1270,8 +1270,6 @@ bubble.addEventListener("touchcancel", () => {
 }
 
 onSnapshot(q, snapshot => {
-  
-const start = performance.now();
 
 chat.innerHTML = "";
 
@@ -1538,9 +1536,6 @@ docs.forEach(docSnap => {
 lucide.createIcons();  
 
 chat.scrollTop = chat.scrollHeight;
-
-const end = performance.now();
-console.log("Render:", (end - start).toFixed(0), "ms");
 
 });
 

f9df88cd4807f5b53c2487e70449e217939b8ec4
christianjuniadi252-ai <christianjuniadi252@gmail.com>
2026-07-05 21:50:14 +0700

    update elements.js

---
+++ elements.js
@@ -0,0 +1,50 @@
+/* ================= ELEMENT ================= */
+
+export const loginBtn = document.getElementById("loginBtn");
+export const userInfo = document.getElementById("userInfo");
+export const avatar = document.getElementById("avatar");
+export const username = document.getElementById("username");
+
+export const chat = document.getElementById("chat");
+export const input = document.getElementById("commandInput");
+export const sendBtn = document.getElementById("sendBtn");
+
+export const player = document.getElementById("playerFrame");
+
+export const replyPreview = document.getElementById("replyPreview");
+export const replyText = document.getElementById("replyText");
+export const cancelReply = document.getElementById("cancelReply");
+
+export const menuOverlay = document.getElementById("menuOverlay");
+export const menuPreview = document.getElementById("menuPreview");
+
+export const copyBtn = document.getElementById("copyBtn");
+export const editBtn = document.getElementById("editBtn");
+export const deleteBtn = document.getElementById("deleteBtn");
+export const replyBtn = document.getElementById("replyBtn");
+
+export const refreshBtn = document.getElementById("refreshBtn");
+
+export const musicHeader = document.getElementById("musicHeader");
+export const musicTitle = document.getElementById("musicTitle");
+export const musicTime = document.getElementById("musicTime");
+export const musicBar = document.getElementById("musicBar");
+
+export const playlistBtn = document.getElementById("playlistBtn");
+export const playlistModal = document.getElementById("playlistModal");
+export const playlistList = document.getElementById("playlistList");
+
+export const scrollBottomBtn =
+    document.getElementById("scrollBottomBtn");
+
+export const commandMenu =
+    document.getElementById("commandMenu");
+
+export const onlineBtn =
+    document.getElementById("onlineBtn");
+
+export const onlineModal =
+    document.getElementById("onlineModal");
+
+export const onlineList =
+    document.getElementById("onlineList");

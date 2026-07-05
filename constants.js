c9e59c711d90bdc17ffa15a012998a2b5ccc3403
christianjuniadi252-ai <christianjuniadi252@gmail.com>
2026-07-05 21:54:36 +0700

    update constants.js

---
+++ constants.js
@@ -0,0 +1,55 @@
+/* ================= COMMAND LIST ================= */
+
+export const commands = [
+
+    {
+        cmd: "/play",
+        desc: "Memutar musik"
+    },
+
+    {
+        cmd: "/pause",
+        desc: "Menjeda musik"
+    },
+
+    {
+        cmd: "/resume",
+        desc: "Melanjutkan musik"
+    },
+
+    {
+        cmd: "/skip",
+        desc: "Lewati lagu"
+    },
+
+    {
+        cmd: "/stop",
+        desc: "Hentikan musik"
+    },
+
+    {
+        cmd: "/clear",
+        desc: "Hapus playlist"
+    },
+
+    {
+        cmd: "/list",
+        desc: "Daftar musik"
+    },
+
+    {
+        cmd: "/list add",
+        desc: "Tambah list"
+    },
+
+    {
+        cmd: "/list delete",
+        desc: "Hapus list"
+    },
+
+    {
+        cmd: "/list rename",
+        desc: "Ganti nama list"
+    }
+
+];

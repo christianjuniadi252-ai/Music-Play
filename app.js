b94a639fae4720f4c2b7b4c211d1989e3b316655
christianjuniadi252-ai <christianjuniadi252@gmail.com>
2026-07-06 22:55:46 +0700

    update app.js

--- app.js
+++ app.js
@@ -2386,6 +2386,35 @@ async function cekVoting(){
     Permainan dibatalkan.`
     
         );
+        
+    const pemain = sambungkataData.pemain;
+    
+    let urutan = "<b>📋 Urutan Pemain</b><br><br>";
+    
+    pemain.forEach((p, index) => {
+    
+        urutan +=
+            `${index + 1}. ${p.nama} ❤️❤️❤️<br>`;
+    
+    });
+    
+    urutan += `
+    <br>
+    Giliran pertama:
+    <b>${pemain[0].nama}</b>
+    
+    <br><br>
+    
+    Huruf:
+    <b>${huruf.toUpperCase()}</b>
+    
+    <br>
+    
+    Waktu:
+    <b>20 detik</b>
+    `;
+    
+    await sendBotMessage(urutan);
     
     }
 
